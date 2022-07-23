64f16a70a2648c6dc87c0460427aadfcefc85067
Márleo Piber da Rosa <marleopr@hotmail.com>
2022-07-23 02:11:55 -0300

    estilizações gerais do site

--- alves-pokedex1/src/pages/homePage/styled-homePage.js
+++ alves-pokedex1/src/pages/homePage/styled-homePage.js
@@ -9,39 +9,69 @@ export const Geral = styled.div`
 `
 
 export const Main = styled.div`
-  background-color: lightgreen;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  background-color: #5E5E5E;
+  justify-content: center;
 `
 
 export const Header = styled.div`
   display: flex;
-  justify-content: space-around;
   align-items: center;
   background-color: white;
+`
+export const LadoEsquerdo = styled.div`
+  display: flex;
+  flex-basis: 42%;
+  /* justify-content: space-around; */
+  /* border: solid orange; */
+`
+
+export const LadoDireito = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  flex-basis: 58%;
 `
 
 export const ImagemTitulo = styled.img`
   width: 307px;
   height: 113px;
-  left: 566px;
-  top: 21px;
 `
 
 export const BotaoPokedex = styled.div`
-  display: flex;
+  display:flex;  
   justify-content: center;
   align-items: center;
-
+  margin: 0 0 0 30px;
   width: 287px;
   height: 74px;
-  background: #33A4F5;
-  border-radius: 8px;
+  color:#fff;
   font-family: 'Poppins', sans-serif;
-  font-style: normal;
   font-weight: 700;
-  font-size: 24px;
-  line-height: 36px;
+  font-size:1.4em;
+  text-align: center;
+  text-decoration:none;
+  border-radius:6px;
+  background-color:#33A4F5;
+  transition: transform 500ms cubic-bezier(0.68, -0.55, 0.265, 1.55), background-position 800ms cubic-bezier(0.68, -0.55, 0.265, 1.55), box-shadow 500ms linear;
+  background-size:contain;
+  background-position: -250px center;
+  background-repeat: no-repeat;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   cursor: pointer;
-  color: #FFFFFF;
+
+:hover {
+  transform: scale(1.1);
+  background-position: -60px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+:active {
+  transform: scale(1);
+  background-position: 500px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+}
 `
 
 export const Grid = styled.div`
@@ -59,6 +89,12 @@ export const Grid = styled.div`
   }
 `
 
+export const BotoesPaginacao = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+`
 export const BotaoPagina = styled.button`
   background-color: ${props => props.value === props.paginaAtual ? "orange" : null};
 `
